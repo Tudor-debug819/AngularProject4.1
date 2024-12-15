b5957ebf7cb5ed74b7dfe71eccb76e3ce20ce22f
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-12-15 18:32:06 +0200

    adding user input

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,5 +11,9 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  onSubmit(titleElement: HTMLInputElement){
+    const enteredTitle=titleElement.value;
+    console.log(enteredTitle);
+  }
 
 }
